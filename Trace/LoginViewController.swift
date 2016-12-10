7b64d94c0a14831d74f48c31fe064cb99ce654b9
Apple <apple@AppledeiMac-97.local>
2016-12-10 18:54:15 +0800

    set font in login page

--- Trace/LoginViewController.swift
+++ Trace/LoginViewController.swift
@@ -2,7 +2,7 @@
 //  LoginViewController.swift
 //  Trace
 //
-//  Created by Apple on 2016/12/9.
+//  Created by Bertram on 2016/12/9.
 //  Copyright © 2016年 Apple. All rights reserved.
 //
 
@@ -10,8 +10,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("loading login view...")
 
         setTextFieldBound() //设置textfiled文字与边框距离
     }
@@ -25,16 +31,18 @@ class LoginViewController: UIViewController {
     func setTextFieldBound(){
         
         //在textfield左边加入一个view的方式设置左边界距离
-        let leftView = UIView.init(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
-        userName.leftView = leftView
+        let leftViewName = UIView.init(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        userName.leftView = leftViewName
         userName.leftViewMode = UITextFieldViewMode.always//模式为始终可见
         
-        passWord.leftView = leftView
+        let leftViewPass = UIView.init(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        passWord.leftView = leftViewPass
         passWord.leftViewMode = UITextFieldViewMode.always
-        
     }
 
+    //是否记住密码
     @IBAction func isCheck(_ sender: UITapGestureRecognizer) {
+        print("check button clicked!")
         if checkTag {
             //已经选中，改为没选中
             rememberImage.image = UIImage(named: "uncheck")
@@ -44,4 +52,11 @@ class LoginViewController: UIViewController {
             checkTag = true
         }
     }
+    
+    //登录按钮点击
+    @IBAction func loginButtonClick(_ sender: UIButton) {
+        
+    }
+    
+    
 }
